c74042874283aeca078058abefb4b257250fdffa
theworldcloud <mathiassrensen514@gmail.com>
2023-09-14 23:07:28 +0200

    removed unsafe lines

--- src/lectio.ts
+++ src/lectio.ts
@@ -24,8 +24,6 @@ function generateLectioCredentials(eventValidation: string): FormData {
     const username = atob(process.env.LECTIO_USERNAME ?? "");
     const password = atob(process.env.LECTIO_PASSWORD ?? "");
 
-    console.log(username, password);
-
     formdata.append("m$Content$username", username ?? "");
     formdata.append("m$Content$password", password ?? "");
     formdata.append("m$Content$passwordHidden", password ?? "");
